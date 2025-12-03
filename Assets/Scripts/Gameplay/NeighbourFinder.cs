f0edd764f6a6649ce88cbc746df4c13c67dcdf47
AzizL <azizlokhandwala12@gmail.com>
2025-12-03 15:41:22 +0530

    Add brute force approch

--- Assets/Scripts/Gameplay/NeighbourFinder.cs
+++ Assets/Scripts/Gameplay/NeighbourFinder.cs
@@ -7,6 +7,7 @@ using System.Numerics;
 public class NeighbourFinder : MonoBehaviour
 {
     [SerializeField] Material nearestSphereMat;
+    [SerializeField] bool useKD;
 
     Dictionary<System.Numerics.Vector3, MeshRenderer> whiteBallCollection = new Dictionary<System.Numerics.Vector3, MeshRenderer>();
 
@@ -42,26 +43,60 @@ public class NeighbourFinder : MonoBehaviour
 
     #endregion
 
+    #region Private
+
+    private void BruteForceFind(System.Numerics.Vector3[] whiteBallVectors, System.Numerics.Vector3 currentPos)
+    {
+        float closestDist = System.Numerics.Vector3.Distance(whiteBallVectors[0], currentPos);
+        float newDist;
+        System.Numerics.Vector3 closestVector = whiteBallVectors[0];
+
+        for (int i = 1; i < whiteBallVectors.Length; i++)
+        {
+            newDist = System.Numerics.Vector3.Distance(whiteBallVectors[i], currentPos);
+
+            if (closestDist > newDist)
+            {
+                closestDist = newDist;
+                closestVector = whiteBallVectors[i];
+            }
+        }
+
+        whiteBallCollection[closestVector].material = nearestSphereMat;
+    }
+
+    #endregion
+
+
     #region Callback
     private void HandleSpawnDone(object arg)
     {
+        UnityEngine.Vector3 currentPosition = transform.position;
         //List<System.Numerics.Vector3> pointList = (List<System.Numerics.Vector3>)arg;
         whiteBallCollection = (Dictionary<System.Numerics.Vector3, MeshRenderer>)arg;
 
-        KdTree kdTree = new KdTree(3, PointArrFromCollection(whiteBallCollection));
+        System.Numerics.Vector3[] whiteBallVectors = PointArrFromCollection(whiteBallCollection);
+        System.Numerics.Vector3 currentPos = new System.Numerics.Vector3(currentPosition.x, currentPosition.y, currentPosition.z);
 
-        UnityEngine.Vector3 currentPosition = transform.position;
+        if (useKD)
+        {
+            KdTree kdTree = new KdTree(3, whiteBallVectors);
 
-        float[] vectorFloatArr = new float[3];
-        vectorFloatArr[0] = currentPosition.x;
-        vectorFloatArr[1] = currentPosition.y;
-        vectorFloatArr[2] = currentPosition.z;
+            float[] vectorFloatArr = new float[3];
+            vectorFloatArr[0] = currentPosition.x;
+            vectorFloatArr[1] = currentPosition.y;
+            vectorFloatArr[2] = currentPosition.z;
 
-        float[] nearestPoint = kdTree.Search(null, vectorFloatArr).point;
-        UnityEngine.Vector3 nearestPointVector = new UnityEngine.Vector3(nearestPoint[0], nearestPoint[1], nearestPoint[2]);
-        whiteBallCollection[new System.Numerics.Vector3(nearestPointVector.x, nearestPointVector.y, nearestPointVector.z)].material = nearestSphereMat;
+            float[] nearestPoint = kdTree.Search(null, vectorFloatArr).point;
+            UnityEngine.Vector3 nearestPointVector = new UnityEngine.Vector3(nearestPoint[0], nearestPoint[1], nearestPoint[2]);
+            whiteBallCollection[new System.Numerics.Vector3(nearestPointVector.x, nearestPointVector.y, nearestPointVector.z)].material = nearestSphereMat;
 
-        Debug.Log("Curr pos: " + currentPosition + " nearest Pos: " + nearestPointVector);
+            Debug.Log("Curr pos: " + currentPosition + " nearest Pos: " + nearestPointVector);
+        }
+        else
+        {
+            BruteForceFind(whiteBallVectors, currentPos);
+        }
     }
     #endregion
 }
